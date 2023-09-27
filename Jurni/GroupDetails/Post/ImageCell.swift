97121f31d859788892e7ef44cbde301562b14e26
Esther Halls <143838428+esthertoolbox@users.noreply.github.com>
2023-09-27 11:29:16 -0600

    Feature/esther group feed (#3)
    * added some conditions so app doesn't crash if trying to pull the wrong kind of data from firebase
    
    * current user avatar image
    
    * collection view for post images added to view controller
    
    * image cell created
    
    * deleted duplicate code
    
    ---------
    
    Co-authored-by: Esther Halls <esthernhalls@gmail.com>

---
+++ Jurni/GroupDetails/Post/ImageCell.swift
@@ -0,0 +1,16 @@
+//
+//  ImageCell.swift
+//  Jurni
+//
+//  Created by Esther on 9/27/23.
+//
+
+import UIKit
+
+class ImageCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    
+    func configure(with imageURL: URL) {
+        
+    }
+}
